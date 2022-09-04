7ecb5c8970a6387e7dc0df29468f4d46f9c10a92
david-commit <david.ondiege@students.moringaschool.com>
2022-09-05 00:35:13 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const submitBtn = document.getElementById('submitBtn')
+
+  submitBtn.addEventListener('click', (e)=> {
+    e.preventDefault()
+
+    const ul = document.getElementById('tasks')
+    const li = document.createElement("li")
+    const inputValue = document.getElementById('new-task-description').value
+    console.log(inputValue)
+    let button = document.createElement("button")
+    button.textContent = "x"
+    button.addEventListener("click", (e) => {
+      li.remove()
+    })
+
+    li.textContent = inputValue
+    ul.append(li)
+    li.appendChild(button)
+    button.style.marginLeft = "10px"
+  
+  })
+
+
+
 });
